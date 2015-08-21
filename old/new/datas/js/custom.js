9abf58312f3a33a9f68269f1c7cc4af8ff3f6b06
lambdainnovphp <mydihemasundar@lambdainnov.com>
2015-08-21 14:01:11 +0530

    scroll
    single scroll

--- old/new/datas/js/custom.js
+++ old/new/datas/js/custom.js
@@ -17,3 +17,26 @@ $(function() {
         event.preventDefault();
     });
 });
+
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 1000;
+    var distance = 1000;
+    
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time );
+}
